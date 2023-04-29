25d7b52f47db0924ca19e46bcbb155b885f95ef5
IdrisFallout <waithakasam2017@gmail.com>
2023-04-29 16:40:49 +0300

    add feedback

--- receiver.py
+++ receiver.py
@@ -1,6 +1,7 @@
 import socket
 
 server_address = ('192.168.1.100', 5000)
+chunk_size = 1024 * 1024
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(server_address)
@@ -9,17 +10,22 @@ client_socket.connect(server_address)
 def receive_data():
     filename = client_socket.recv(1024).decode()
     file_path = 'destination/' + filename
-    print(file_path)
+    print("(Receiving)")
+    count = 0
     with open(file_path, 'wb') as file:
         while True:
-            data = client_socket.recv(1024)
+            data = client_socket.recv(chunk_size)
             if not data:
                 break
             file.write(data)
-            print(data)
+            count += 1
+            print(".", end="")
+            if count % 50 == 0:
+                print("")
+
 
     client_socket.close()
-    print("File received successfully")
+    print("\nFile received successfully")
 
 
 receive_data()
--- sender.py
+++ sender.py
@@ -3,8 +3,9 @@ import socket
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-host = '192.168.1.100'
+host = '0.0.0.0'
 port = 5000
+chunk_size = 1024 * 1024
 
 client_socket.bind((host, port))
 
@@ -12,13 +13,20 @@ client_socket.bind((host, port))
 def make_request(file_path, conn):
     filename = os.path.basename(file_path)
     conn.sendall(filename.encode())
+    print("(Sending)")
+    count = 0
     with open(file_path, 'rb') as file:
         while True:
-            data = file.read(1024)
+            data = file.read(chunk_size)
             if not data:
                 break
             conn.send(data)
-        print("File sent successfully")
+            count += 1
+            print(".", end="")
+            if count % 50 == 0:
+                print("")
+
+        print("\nFile sent successfully")
     client_socket.close()
 
 
@@ -27,7 +35,7 @@ def parse_data():
     conn, addr = client_socket.accept()
     print("Connected by", addr)
 
-    make_request('D:\Multimedia\The Lion King (2019) [BluRay] [1080p] [YTS.LT]\The.Lion.King.2019.1080p.BluRay.x264-[YTS.LT].mp4', conn)
+    make_request(r'D:\Multimedia\Rick.and.Morty.S06 Complete\Rick.and.Morty.S06E09.1080p.x265-ELiTE.mkv', conn)
 
 
 parse_data()
